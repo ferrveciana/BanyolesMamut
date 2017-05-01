74ad3a09d7d50a89cbc1822c814738802879bd64
Ferran <ferrveciana@gmail.com>
2017-05-01 18:17:43 +0200

    Add files via upload

--- Visitable.java
+++ Visitable.java
@@ -1,6 +1,8 @@
 package prop;
 
+import java.util.HashMap;
 import java.util.List;
+
 import java.util.TimeZone;
 
 
@@ -13,7 +15,7 @@ public class Visitable implements Secundari{
     private final double preu;
     private final List<String> caracteristiques;
     private final List<Horari> horariVisites;
-    private SortedMap<String,> excepcions; 
+    private HashMap<String,DuesHores> excepcions;
     
     public Visitable(String _nom, double _coordX, double _coordY, TimeZone _zonaHoraria, int _tempsVisita, double _preu, List<String> _caract, List<Horari> _horariVisites){
         nom=_nom;
