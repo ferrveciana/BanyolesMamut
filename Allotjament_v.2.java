527525c20d5083a424b0292a8f2a26b28c650e2d
wpveby <narcisbustins@gmail.com>
2017-05-01 18:58:04 +0200

    Create Allotjament_v.2.java

---
+++ Allotjament_v.2.java
@@ -0,0 +1,27 @@
+package proactiva;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.TimeZone;
+
+/**
+ *
+ * @author narcisbustins
+ */
+public class Allotjament extends PuntInteres{
+  
+    private final int categoria;
+
+    public Allotjament(String _nom, String _coordenada, TimeZone _zonaHoraria, double _preu, List<String> _caract,int _categoria){
+        
+        super(_nom, _coordenada, _zonaHoraria, _preu, _caract);
+        categoria = _categoria;
+        
+    }
+
+    @Override
+    public String tipus() {
+        return ("Allotjament");
+    }
+    
+}
