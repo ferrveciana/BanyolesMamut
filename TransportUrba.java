09b256e8e8fbca23af4049202a05665993bad757
Ferran <ferrveciana@gmail.com>
2017-05-03 19:38:06 +0200

    Add files via upload

---
+++ TransportUrba.java
@@ -0,0 +1,16 @@
+package prop;
+
+
+public class TransportUrba {
+    private final String lloc;
+    private final String mitja;
+    private final int durada;
+    private final float preu;
+    
+    public TransportUrba(String _lloc, String _mitja, int _durada, float _preu){
+        lloc=_lloc;
+        mitja=_mitja;
+        durada=_durada;
+        preu=_preu;
+    }
+}
