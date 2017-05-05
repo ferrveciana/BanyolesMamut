8cde103a4b837c2ff5498bda1e2c795ba57de8b2
Ferran <ferrveciana@gmail.com>
2017-05-05 11:19:48 +0200

    Update Greedy.java

--- Greedy.java
+++ Greedy.java
@@ -1,39 +1,29 @@
-Atributs:
 private final Circuit circuit;
 private final Lloc desti;
 private final List<Lloc> llocsIntermitjos;
 private finalList<boolean> passat; //inicialitzar amb llocsIntermitjos.size() posicions i totes a false.
 private final boolean hiHaSolucio;
 
-Algoritme:
-Public void ferGreedy(Lloc inici) {
+public void camiMesCurtPassantPerLlocs(Lloc ini, Lloc fi, Llista<PuntRuta> llocsIntermitjos){
 
-	  Lloc actual=inici;
-
-	  while (!circuit.completa(desti) and Candidats.hiHaSeguent(actual)) {
-
-		Lloc millorLloc=Candidats.mesPrometedor(actual);
-		Candidats.treure(millorLloc);
-
-		circuit.anotar(millorLloc);
-	  actualitzarLlocsPassats(millorLloc);
-		actual=millorLloc;
-  }
+   	int millorTemps=0;
+Ruta millorRuta;
+	int nCurt=0,n=0;
 	
-	if (circuit.completa(desti) and totsPuntsIntermitjosVisitats())
-		  hiHaSolucio=true;
-	else 
-		  hiHaSolucio=false;
+	while(llocsIntermitjos[n] != NULL){
+		ruta=camiMesCurt(ini,llocsIntermitjos[n]);
+		if(millorRuta.temps>ruta.temps){ //lu del temps ho he posat aixi pk no se molt be com ho tenim en el dijkstra
+			millorRuta = ruta;
+			nCurt=n;
+		}
+		n++
+	}
+	sol=sol+millorRuta; //concatenar rutes
+	ini= llocsIntermitjos[nCurt];
+	llocsIntermitjos.remove(nCurt);	
+	if(! llocsIntermitjos.isEmpty()) camiMesCurtPassantPerLlocs(ini,fi,llocsIntermitjos);
+	else{
+		ruta=camiMesCurt(ini, fi);
+		sol=sol+ruta; //concatenar rutes
+	}
 }
-
-Classe actual:
-void actualitzarLlocsPassats(millorLloc); //Llista passats actualitzada si millorLloc existeix a llista llocsIntermitjos
-Void totsPuntsIntermitjosVisitats(); //Cert si s’han visitat tots els punts intermitjos
-
-Circuit:
-void anotar(Lloc millorLloc);
-boolean Completa(Lloc desti);
-
-Candidats:
-boolean hiHaSeguent(actual); //retorna si el lloc actual al que estem té “veïns”
-void treure(Lloc millorLloc);
