6640a1516a2ca1d4258a96854805670d0c5de199
alexsegura3 <alexsegurasoler@gmail.com>
2017-05-06 16:09:47 +0200

    Create Viatge.java

---
+++ proActiva/src/proactiva/Viatge.java
@@ -0,0 +1,26 @@
+package proactiva;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Viatge {
+
+  private List<String> clients;
+  private List<String> rutes;
+  private 
+  
+  public Viatge (LocalDate _dataInici, String _horaInici, int _nombreDies, double _preuMaxim, String _categoria, List<String> _clients, List<String> _rutes) {
+        
+      dataInici = _dataInici;
+      horaInici = _horaInici;
+      nombreDies = _nombreDies;
+      preuMaxim = _preuMaxim;
+      categoria = _categoria;
+      clients = _clients;
+      rutes = _rutes;
+  }
+  
+}
