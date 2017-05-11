ebd5c18287b9ff05239287518fe1723df5fbd123
Ferran <ferrveciana@gmail.com>
2017-05-12 00:01:51 +0200

    Add files via upload

---
+++ greedy_v1.java
@@ -0,0 +1,27 @@
+
+//DON'T GET GREEDY
+
+//son
+
+
+public void cobdicia (){
+	
+	while(actual != desti && !acaba){
+		List<Activitat> llistaActivitat = 
+		
+		Candidats candidat = solucio.inicialitzarCandidats(pi);
+		Activitat millor = candidat.mesPrometedor(parametre);
+		
+		llistaActivitat.add(millor);
+		
+		acaba = millor.piActual().equals(desti);
+		
+	}
+	
+	
+	
+	
+	inicialitzacions noseque
+	mentre no acaba
+	agafar el mes prometedor
+}
