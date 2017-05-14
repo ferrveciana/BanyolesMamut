dd85d303fbc73664f731431225a155ce6aabc146
alexsegura3 <alexsegurasoler@gmail.com>
2017-05-14 21:41:56 +0200

    Add files via upload

--- proActiva/src/proactiva/Agencia.java
+++ proActiva/src/proactiva/Agencia.java
@@ -1,3 +1,4 @@
+
 package proactiva;
 
 import java.time.LocalDate;
@@ -85,7 +86,7 @@ public class Agencia {
         System.out.println("AQUET " + secundari);
         Ciutat ciutat;
         if (_lloc instanceof Ciutat){ //prèviament convertit a ciutat 
-            ciutat = (Ciutat) _lloc;
+            ciutat = new Ciutat(_lloc.nom, _lloc.getCoordenada(),_lloc.getZonaHoraria());
         }
         else {
             ciutat = _lloc.ferCiutat(pi);
