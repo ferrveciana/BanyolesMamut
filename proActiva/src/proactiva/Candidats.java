8bd17aafff31185fc465043a33f1802f1c4754df
alexsegura3 <alexsegurasoler@gmail.com>
2017-05-16 14:52:45 +0200

    Update Candidats.java

--- proActiva/src/proactiva/Candidats.java
+++ proActiva/src/proactiva/Candidats.java
@@ -18,9 +18,41 @@ public class Candidats implements Iterable<Activitat>{
     private int index; //Indica la posicio del candidat actual
     private List<Activitat> llistaActivitats; 
     
-   Candidats (PuntInteres _pi){
+   	Candidats (PuntInteres _pi){
         
-       llistaActivitats = new ArrayList();
+		llistaActivitats = new ArrayList();
+		
+		//Crear candidats amb transports directes
+		List<TransportDirecte> llistaTransportsDirectes = _pi.obtenirTransportsDirectes();
+
+		for (int i = 0; i < llistaTransportsDirectes.size(); i++) {
+			Desplaçament desplaçamentDirecte = new Desplaçament(llistaTransportsDirectes[i].obtenirDurada(),_pi,llistaTransportsDirectes[i].obtenirDesti(),llistaTransportsDirectes[i].obtenirPreu(),"transport Directe");
+			llistaActivitats.add(desplaçamentDirecte); //falta afegir un string per saber quin tipus de transport es tracta
+		}
+		
+		//Crear candidats amb transports urbans
+		String ciutat = _pi.getCiutat(); //fer try catch
+		List<TransportUrba> transportsUrbans = ciutat.obtenirTransports();
+		List<PuntInteres> puntsInteres = _pi.getCiutat().obtenirPuntInteres();
+		
+		for (int i = 0; i < transportsUrbans.size(); i++) { //per cada transport urba de la ciutat
+			for (int j = 0; j < puntsInteres.size(); j++) { //per cada punt d'interes de la ciutat
+				//Afegir transports urbans del punt d'Interes actual a la resta de punts d'Interes de la ciutat del _pi
+				Desplaçament desplaçamentUrba = new Desplaçament(transportsUrbans[i].obtenirDurada(),_pi,puntsInteres[j],transportsUrbans[i].obtenirPreu(),"transport Urba");
+				llistaActivitats.add(desplaçamentUrba);
+			}
+		}
+		
+		List<Hub> hubs = ciutat.obtenirHub();
+		
+		for (int i = 0; i < hubs.size(); i++) {
+			//obtenir llista de transportsIndirectes de hubs[i] i crear una activitat per cada un
+			
+		}
+		
+			  
+		
+
        //Crear totes les activitats
     }
 
