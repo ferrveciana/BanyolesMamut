03182d77b316f6613f83492aee0b44a3ccf9fea8
alexsegura3 <alexsegurasoler@gmail.com>
2017-05-22 15:30:05 +0200

    ********************FALTA BRIEF DE LA CLASSE

--- proActiva/src/proactiva/Activitat.java
+++ proActiva/src/proactiva/Activitat.java
@@ -8,41 +8,71 @@ package proactiva;
 import java.time.LocalDateTime;
 import java.util.List;
 
-/**
- *
- * @author narcisbustins
+/*
+ * @class Activitat
+ * @brief 
  */
 public abstract class Activitat {
     
-    private int durada;
-    private float preu;
-    private LocalDateTime hinici;
-    private LocalDateTime hfinal;
+    private int durada; ///< durada total de l'Activitat
+    private float preu; ///< preu de l'Activitat
+    private LocalDateTime hinici; ///< hora d'inici de l'Activitat
+    private LocalDateTime hfinal; ///< hora final de l'Activitat
     
+   /**
+    * @brief actualitza la durada i el preu de l'Activitat
+    * @pre cert
+    * @post durada i preu actualitzats
+    */
     Activitat(int _durada,float _preu){
         
        durada = _durada;
        preu = _preu;
     }
     
+   /**
+    * @brief retorna la durada de l'Activitat
+    * @pre cert
+    * @post return durada
+    */
     public int durada(){
         
         return durada;
     }
     
+   /**
+    * @brief retorna el preu de l'Activitat
+    * @pre cert
+    * @post return preu
+    */
     public float preu(){
         
         return preu;
     }
     
+   /**
+    * @brief actualitza la hora inici de l'Activitat
+    * @pre cert
+    * @post hinici = _inici
+    */
     public void setInici(LocalDateTime _inici){
         hinici=_inici;
     }
     
+   /**
+    * @brief actualitza la hora final de l'Activitat
+    * @pre cert
+    * @post hfinal = _final
+    */
     public void setFinal(LocalDateTime _final){
         hfinal=_final;
     }
     
+   /**
+    * @brief retorna la hora final de l'Activitat
+    * @pre cert
+    * @post return hfinal
+    */
     public LocalDateTime getFinal(){
         return hfinal;
     }
