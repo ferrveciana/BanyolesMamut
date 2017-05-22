e123dda58167a3bd15abdafd70cb60d9abdb6862
alexsegura3 <alexsegurasoler@gmail.com>
2017-05-22 15:59:56 +0200

    Update Client.java

--- proActiva/src/proactiva/Client.java
+++ proActiva/src/proactiva/Client.java
@@ -10,30 +10,50 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+ * @class Client
+ * @brief Classe que guarda les dades d'un client
+ */
 public class Client {
     
-    private final String nom;
-    private final ArrayList<String> prefList;
-    private Map<String,LocalDate> visites;
+    private final String nom; ///< nom del Client
+    private final ArrayList<String> prefList; ///< llista de preferencies del Client
+    private Map<String,LocalDate> visites; ///< llista de visites que el Client ha realitzat anteriorment
     
-    ///@pre:
-    ///@post: s'ha creat un client a partir de les dades entrades
+   /**
+     * @brief constructor de Client a partir de les dades que li passem
+     * @pre cert
+     * @post Client creat
+     */
     public Client(String _nom, ArrayList<String> _prefList){
         nom=_nom;
         prefList=_prefList;
         visites = new HashMap<String,LocalDate>();
     }
     
-    ///@pre: ---
-    ///@post: retorna una llista d’strings amb les preferències de l’objecte actual
+   /**
+     * @brief retorna la llista de preferencies de Client
+     * @pre cert
+     * @post return prefList
+     */
     public ArrayList<String> obtPreferencies(){
         return prefList;
     }
     
+   /**
+     * @brief retorna la llista de visites de Client
+     * @pre cert
+     * @post return visites
+     */
      public Map<String,LocalDate> obtVisites(){
         return visites;
     }
      
+   /**
+     * @brief afegeix una visita que ha realitzat anteriorment el Client a la llista de visites
+     * @pre cert
+     * @post visita afegida a visites
+     */
     public void afegirVisita(String nomlloc,LocalDate data){
         visites.put(nomlloc, data);
     }
