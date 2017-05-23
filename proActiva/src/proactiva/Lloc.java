180b68719fe21ec31e3bf74bc9e640bf1724b1cb
wpveby <narcisbustins@gmail.com>
2017-05-23 12:39:18 +0200

    Update Lloc.java

--- proActiva/src/proactiva/Lloc.java
+++ proActiva/src/proactiva/Lloc.java
@@ -11,6 +11,11 @@ public class Lloc {
    private final String coordenada;
    private final TimeZone zonaHoraria;
    
+   /**
+     * @brief Crea un objecte lloc
+     * @pre cert
+     * @post Lloc creat a partir de nom coordenada i zonahoraria
+     */ 
    Lloc(String _nom, String _coordenada, TimeZone _zonaHoraria){
        
         nom=_nom;
@@ -19,6 +24,11 @@ public class Lloc {
         
    }
    
+    /**
+     * @brief Crea un Lloc buit
+     * @pre cert
+     * @post S'ha creat un lloc buit
+     */
    Lloc() {
        nom = "";
        coordenada = "";
@@ -26,14 +36,29 @@ public class Lloc {
        
    }
    
+   /**
+     * @brief Retorna les coordenades de Lloc
+     * @pre Lloc no es buit
+     * @post retorna les coordenades de this
+     */ 
    public String getCoordenada() {
        return coordenada;
    }
    
+    /**
+     * @brief Retorna la zona horaria de Lloc
+     * @pre Lloc no es buit
+     * @post retorna la zona horaria de this
+     */ 
    public TimeZone getZonaHoraria() {
        return zonaHoraria;
    }
    
+     /**
+     * @brief Retorna una ciutat creada a partir de Lloc i un PuntInteres
+     * @pre Lloc no es buit
+     * @post Retorna la ciutat creada amb els atributs de lloc i conte secundari dins la llista de punts d'interes
+     */
    Ciutat ferCiutat(PuntInteres secundari){
        
        Ciutat city = new Ciutat(nom,coordenada,zonaHoraria);
@@ -42,6 +67,11 @@ public class Lloc {
        return city;
    }
    
+     /**
+     * @brief Retorna el nom de Lloc
+     * @pre cert
+     * @post Retorna el nom de Lloc
+     */
     public String getNom(){
         
         return nom;
